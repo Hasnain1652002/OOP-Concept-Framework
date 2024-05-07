036d6da9aead0b12ddf7001c6ae13298dc73daf0
Hasnain <112103324+Hasnain1652002@users.noreply.github.com>
2024-05-08 00:49:54 +0500

    Add files via upload

---
+++ swap.cpp
@@ -0,0 +1,23 @@
+#include"bits\stdc++.h"
+using namespace std;
+
+int main(){
+    
+    int x = 5;
+    int y = 6;
+    
+    x = x + y;
+    cout << "x: " << x << endl;
+
+    y = x - y;
+    cout << "y: " << y << endl;
+
+    x = x - y;
+    cout << "x: " << x << endl;
+
+
+    cout << "x: " << x << endl;
+    cout << "y: " << y << endl;
+    
+    return 0;
+}
